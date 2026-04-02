880e4aa867e50b2a38f67fe53db4b8f48e0da408
luca spolladore <luca.spolladore@outlook.it>
2026-04-02 21:35:19 +0200

    Add BuildReviewQueue with cluster inheritance for categorization

--- categorize.go
+++ categorize.go
@@ -6,19 +6,30 @@ type ReviewItem struct {
 }
 
 func BuildReviewQueue(transactions []Transaction) []ReviewItem {
-	uniqueDescriptions := make(map[string]struct{})
+	uniqueDescriptions := make(map[string]string)
 	for _, t := range transactions {
-		if t.Category != CategoryUncategorized {
-			uniqueDescriptions[t.Description] = struct{}{}
-		}
+		uniqueDescriptions[t.Description] = t.Category
 	}
 	uniqueList := make([]string, 0, len(uniqueDescriptions))
 	for desc := range uniqueDescriptions {
 		uniqueList = append(uniqueList, desc)
 	}
 	clusters := ClusterDescriptions(uniqueList, 0.8)
-	reviewQueues := make([]ReviewItem, 0, len(clusters))
+	uncategorizedClusters := make([][]string, 0)
 	for _, cluster := range clusters {
+		categorized := false
+		for _, desc := range cluster {
+			if uniqueDescriptions[desc] != CategoryUncategorized {
+				categorized = true
+				break
+			}
+		}
+		if !categorized {
+			uncategorizedClusters = append(uncategorizedClusters, cluster)
+		}
+	}
+	reviewQueues := make([]ReviewItem, 0, len(uncategorizedClusters))
+	for _, cluster := range uncategorizedClusters {
 		reviewQueues = append(
 			reviewQueues,
 			ReviewItem{label: clusterLabel(cluster), descriptions: cluster},
